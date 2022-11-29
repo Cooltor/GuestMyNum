af24c0f8afb9cceb1a1105e75daca401a4d5f450
Cooltor <romaindbus86@gmail.com>
2022-11-29 16:42:32 +0100

    Score done

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
 "use strict";
-/*
-document.querySelector(".message").textContent = "Correct Number!🏆";
-
-document.querySelector(".number").textContent = 13;
-document.querySelector(".score").textContent = 10;
-
-document.querySelector(".guess").value = 23;
-
-*/
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
