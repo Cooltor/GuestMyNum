806f03b5d46fa2809780b81b395c963f3336b1ef
Cooltor <romaindbus86@gmail.com>
2022-11-30 12:02:33 +0100

    Done

--- script.js
+++ script.js
@@ -48,6 +48,7 @@ document.querySelector(".check").addEventListener("click", function () {
 
 document.querySelector(".again").addEventListener("click", function () {
   score = 20;
+
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   document.querySelector(".score").textcontent = score;
   document.querySelector(".number").textContent = "?";
